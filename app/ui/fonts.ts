46411027a2bfc6178df45b9cc02af9938f3435c2
Jonatas <jonbranaraujo18@gmail.com>
2024-04-29 16:03:08 -0300

    "Add global CSS, update fonts, and add hero images to the dashboard page"
    This commit includes the following changes:
    
    1. Adds `@/app/ui/global.css` and `@/app

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter(
+  { 
+    subsets: ['latin'], 
+    display: 'swap', 
+  }
+);
+
+export const lusitana = Lusitana(
+  { 
+    subsets: ['latin'], 
+    weight: ["400"], 
+    display: 'swap', 
+  }
+);
